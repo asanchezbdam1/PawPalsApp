f0f2b6919d02af34ef9b49ee3e996749b2dafc16
Asier <66691307+ASX21@users.noreply.github.com>
2022-02-01 21:23:19 +0100

    Introducida conexión básica al servidor

--- PawPalsApp/PawPalsApp/Classes/ConnectionHelper.cs
+++ PawPalsApp/PawPalsApp/Classes/ConnectionHelper.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace PawPalsApp.Classes
 {
     public class ConnectionHelper
     {
+        private const int PORT = 12012;
+        public static void StartConnection()
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse("192.168.1.19");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, PORT);
+                Socket client = new Socket(ipAddress.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
+                client.BeginConnect(remoteEP,
+                        new AsyncCallback((IAsyncResult ar) => { }), client);
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
