3637b2e10fe958d3e8ab763736c98617f87a0fc9
Asier <66691307+ASX21@users.noreply.github.com>
2022-02-03 19:39:07 +0100

    Commit erróneo antes de rollback

--- PawPalsApp/PawPalsApp/Views/Login.xaml.cs
+++ PawPalsApp/PawPalsApp/Views/Login.xaml.cs
@@ -27,7 +27,7 @@ namespace PawPalsApp.Views
 
         private void btnLogin_Clicked(object sender, EventArgs e)
         {
-            User user = new User()
+            User user = new LoginUser()
             {
                 Login = txtUser.Text,
                 Pwd = txtPwd.Text
--- PawPalsServer/ServerActions/ServerActions.cs
+++ PawPalsServer/ServerActions/ServerActions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ServerActions
 {
-    public class Class1
+    public class ServerActions
     {
     }
 }
