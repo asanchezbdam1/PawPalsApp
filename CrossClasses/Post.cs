f06151cb36fb9be720f8d714927ef72bd1dbc007
Asier <66691307+ASX21@users.noreply.github.com>
2022-02-04 16:53:22 +0100

    Modificado sql para crear las tablas de la base de datos

--- CrossClasses/Post.cs
+++ CrossClasses/Post.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,18 @@ namespace CrossClasses
     [Serializable]
     public class Post
     {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public Bitmap Image { get; set; }
+
+        public Post()
+        {
+            ID = 0;
+            Username = String.Empty;
+            Likes = 0;
+            Dislikes = 0;
+        }
     }
 }
