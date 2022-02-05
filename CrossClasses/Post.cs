4cf7b783127c2ac6e9dd42568baa110e1c35c60f
Asier <66691307+ASX21@users.noreply.github.com>
2022-02-05 22:49:04 +0100

    Añadida estructura para probar el monómetro

--- CrossClasses/Post.cs
+++ CrossClasses/Post.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
 
 namespace CrossClasses
 {
@@ -14,7 +13,8 @@ namespace CrossClasses
         public string Username { get; set; }
         public int Likes { get; set; }
         public int Dislikes { get; set; }
-        public BitmapImage Image { get; set; }
+        public byte[] Img { get; set; }
+        public double Ratio { get { return Likes / (Likes + Dislikes); } }
 
         public Post()
         {
--- PawPalsApp/PawPalsApp/Views/CuteOMeter.xaml.cs
+++ PawPalsApp/PawPalsApp/Views/CuteOMeter.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CrossClasses;
+using PawPalsApp.Classes;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +16,40 @@ namespace PawPalsApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CuteOMeter : ContentPage
     {
+        private ObservableCollection<Post> posts;
         public CuteOMeter()
         {
             InitializeComponent();
+            posts = new ObservableCollection<Post>();
+            lvPosts.ItemsSource = posts;
+            List<Post> list = new List<Post>();
+            using (var webClient = new WebClient())
+            {
+                var img = webClient.DownloadData("http://www.google.com/images/logos/ps_logo2.png");
+                Post post = new Post
+                {
+                    Username = "Prueba",
+                    Likes = 10,
+                    Dislikes = 3
+                };
+                post.Img = img;
+                list.Add(post);
+                list.Add(new Post
+                {
+                    Username = "Prueba2",
+                    Likes = 10,
+                    Dislikes = 3,
+                    Img = img
+                });
+                list.Add(new Post
+                {
+                    Username = "Prueba3",
+                    Likes = 10,
+                    Dislikes = 3,
+                    Img = img
+                });
+            }
+            DisplayAlert("La concha de la lora", "FUNCIONA", "VOLVER");
         }
     }
 }
