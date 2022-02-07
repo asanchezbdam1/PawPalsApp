9a67ad7e4fdc7efd85a33e8f74f5bc494f7fe7bf
dsuescufer <dsuescufer@educacion.navarra.es>
2022-02-07 17:53:11 +0100

    Clase mascota

---
+++ PawPalsApp/PawPalsApp/Classes/Mascotas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawPalsApp.Classes
+{
+    internal class Mascotas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public List<String> Ejercicios { get; set; }
+        public List<String> Higiene { get; set; }
+        public List<String> Dieta { get; set; }
+
+    }
+}
