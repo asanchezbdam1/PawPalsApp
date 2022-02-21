5aaf68fdaaa338bc3b0734d801e48cc4a99d76ab
dsuescufer <dsuescufer@educacion.navarra.es>
2022-02-21 19:10:19 +0100

    Data Comentado

--- PawPalsApp/PawPalsApp/Data/SQLiteDieta.cs
+++ PawPalsApp/PawPalsApp/Data/SQLiteDieta.cs
@@ -10,12 +10,22 @@ namespace PawPalsApp.Data
     public class SQLiteDieta
     {
         SQLiteAsyncConnection db;
-
+        /// <summary>
+        /// Metodo que crea una nueva tabla para guardar dietas
+        /// </summary>
+        /// <param name="dbPath"></param>
         public SQLiteDieta(string dbPath)
         {
             db = new SQLiteAsyncConnection(dbPath);
             db.CreateTableAsync<Dieta>().Wait();
         }
+
+        /// <summary>
+        /// Metodo que en base a un objeto dieta este se guarda en la
+        /// tabla o se sobreescribe 
+        /// </summary>
+        /// <param name="dieta"></param>
+        /// <returns></returns>
         public Task<int> SaveDietaAsync(Dieta dieta)
         {
             if (dieta.Id == 0)
@@ -27,15 +37,31 @@ namespace PawPalsApp.Data
                 return db.UpdateAsync(dieta);
             }
         }
+        /// <summary>
+        /// Metodo que devuelve la lista de objetos dieta en la tabla
+        /// </summary>
+        /// <returns></returns>
         public Task<List<Dieta>> GetDietaAsync()
         {
             return db.Table<Dieta>().ToListAsync();
         }
+
+        /// <summary>
+        /// Devuelve unas dietas dependiendo del nombre de mascota que se la pase como parametro
+        /// </summary>
+        /// <param name="nombreMascota"></param>
+        /// <returns></returns>
         public Task<List<Dieta>> GetDietaByMascotaAsync(string nombreMascota)
         {
             return
            db.Table<Dieta>().Where(p => p.IdMascota.Equals(nombreMascota)).ToListAsync();
         }
+
+        /// <summary>
+        /// Elimina la dieta de la tabla pasada como parametro
+        /// </summary>
+        /// <param name="dieta"></param>
+        /// <returns></returns>
         public Task<int> DeleteDietaAsync(Dieta dieta)
         {
             return db.DeleteAsync(dieta);
--- PawPalsApp/PawPalsApp/Data/SQLiteEjercicio.cs
+++ PawPalsApp/PawPalsApp/Data/SQLiteEjercicio.cs
@@ -10,12 +10,22 @@ namespace PawPalsApp.Data
     public class SQLiteEjercicio
     {
         SQLiteAsyncConnection db;
-
+        /// <summary>
+        /// Metodo que crea una nueva tabla para guardar ejercicios
+        /// </summary>
+        /// <param name="dbPath"></param>
         public SQLiteEjercicio(string dbPath)
         {
             db = new SQLiteAsyncConnection(dbPath);
             db.CreateTableAsync<Ejercicios>().Wait();
         }
+
+        /// <summary>
+        /// Metodo que en base a un objeto ejercicios este se guarda en la
+        /// tabla o se sobreescribe 
+        /// </summary>
+        /// <param name="ejercicio"></param>
+        /// <returns></returns>
         public Task<int> SaveEjercicioAsync(Ejercicios ejercicio)
         {
             if (ejercicio.Id == 0)
@@ -27,15 +37,32 @@ namespace PawPalsApp.Data
                 return db.UpdateAsync(ejercicio);
             }
         }
+
+        /// <summary>
+        /// Metodo que devuelve la lista de objetos ejercicios en la tabla
+        /// </summary>
+        /// <returns></returns>
         public Task<List<Ejercicios>> GetEjercicioAsync()
         {
             return db.Table<Ejercicios>().ToListAsync();
         }
+
+        /// <summary>
+        /// Devuelve unos ejercicios dependiendo del nombre de mascota que se la pase como parametro
+        /// </summary>
+        /// <param name="nombreMascota"></param>
+        /// <returns></returns>
         public Task<List<Ejercicios>> GetEjercicioByMascotaAsync(string nombreMascota)
         {
             return
            db.Table<Ejercicios>().Where(p => p.IdMascota.Equals(nombreMascota)).ToListAsync();
         }
+
+        /// <summary>
+        /// Elimina el Ejercicio de la tabla pasada como parametro
+        /// </summary>
+        /// <param name="ejercicio"></param>
+        /// <returns></returns>
         public Task<int> DeleteEjercicioAsync(Ejercicios ejercicio)
         {
             return db.DeleteAsync(ejercicio);
